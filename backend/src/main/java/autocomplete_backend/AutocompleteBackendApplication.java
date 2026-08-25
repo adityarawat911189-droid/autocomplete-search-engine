a8e59fa1686a64c9a98d8205381d754d6dc20af1
Aditya Rawat <adityarawat911189@gmail.com>
2026-08-25 10:08:29 +0530

    Restructure project: organize into core/ and backend/ folders

---
+++ backend/src/main/java/autocomplete_backend/AutocompleteBackendApplication.java
@@ -0,0 +1,13 @@
+package autocomplete_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocompleteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocompleteBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/autocomplete_backend/AutocompleteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package autocomplete_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocompleteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
